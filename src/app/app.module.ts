b71eaf356ed03e7ab0e3086f3e858729a00c16c1
benoumar21 <benoumar21@gmail.com>
2017-04-02 10:44:48 +0200

    ajout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    MaterialModule.forRoot(),
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
